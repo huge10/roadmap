fb2f9559c552cfd126f704c9352d38931a6aa945
huge10 <35537393+huge10@users.noreply.github.com>
2025-05-09 15:47:48 +0200

    Disable regular login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,52 +23,55 @@
                     </div>
                 @endif
 
+               
                 <form class="mt-8 space-y-6 md:mt-12"
                       method="post"
                       action="{{ route('login') }}">
                     @csrf
-                    <div class="space-y-2">
-                        <label class="inline-block text-sm font-medium text-gray-700"
-                               for="email">{{ trans('auth.email') }}</label>
-
-                        <input
-                            class="block w-full h-10 transition duration-75 border-gray-300 rounded-lg shadow-sm focus:ring-1 focus:ring-inset focus:ring-brand-600 focus:border-brand-600 dark:bg-gray-900 dark:border-white/10"
-                            id="email"
-                            name="email"
-                            value="{{ old('email') }}"
-                            type="email">
-                    </div>
-
-                    <div class="space-y-2">
-                        <label class="inline-block text-sm font-medium text-gray-700"
-                               for="password">{{ trans('auth.password') }}</label>
-
-                        <input
-                            class="block w-full h-10 transition duration-75 border-gray-300 rounded-lg shadow-sm focus:ring-1 focus:ring-inset focus:ring-brand-600 focus:border-brand-600 dark:bg-gray-900 dark:border-white/10"
-                            id="password"
-                            name="password"
-                            type="password">
-                    </div>
-
-                    <div class="flex space-x-3">
-                        <input
-                            class="text-brand-500 transition border border-gray-300 rounded shadow-sm focus:border-brand-500 focus:ring-2 focus:ring-brand-500"
-                            id="remember"
-                            name="remember"
-                            type="checkbox">
-
-                        <div class="flex flex-col space-y-1">
-                            <label class="inline-block text-sm font-medium leading-4 text-gray-700"
-                                   for="remember">
-                                {{ trans('auth.remember_me') }}
-                            </label>
+                    @if( app(\App\Settings\GeneralSettings::class)->disable_user_registration )
+                        <div class="space-y-2">
+                            <label class="inline-block text-sm font-medium text-gray-700"
+                                   for="email">{{ trans('auth.email') }}</label>
+    
+                            <input
+                                class="block w-full h-10 transition duration-75 border-gray-300 rounded-lg shadow-sm focus:ring-1 focus:ring-inset focus:ring-brand-600 focus:border-brand-600 dark:bg-gray-900 dark:border-white/10"
+                                id="email"
+                                name="email"
+                                value="{{ old('email') }}"
+                                type="email">
                         </div>
-                    </div>
-
-                    <button
-                        class="flex items-center justify-center w-full h-8 px-3 text-sm font-semibold tracking-tight text-white transition bg-brand-600 rounded-lg shadow hover:bg-brand-500 focus:bg-brand-700 focus:outline-none focus:ring-offset-2 focus:ring-offset-brand-700 focus:ring-2 focus:ring-white focus:ring-inset"
-                        type="submit">{{ trans('auth.login') }}
-                    </button>
+    
+                        <div class="space-y-2">
+                            <label class="inline-block text-sm font-medium text-gray-700"
+                                   for="password">{{ trans('auth.password') }}</label>
+    
+                            <input
+                                class="block w-full h-10 transition duration-75 border-gray-300 rounded-lg shadow-sm focus:ring-1 focus:ring-inset focus:ring-brand-600 focus:border-brand-600 dark:bg-gray-900 dark:border-white/10"
+                                id="password"
+                                name="password"
+                                type="password">
+                        </div>
+    
+                        <div class="flex space-x-3">
+                            <input
+                                class="text-brand-500 transition border border-gray-300 rounded shadow-sm focus:border-brand-500 focus:ring-2 focus:ring-brand-500"
+                                id="remember"
+                                name="remember"
+                                type="checkbox">
+    
+                            <div class="flex flex-col space-y-1">
+                                <label class="inline-block text-sm font-medium leading-4 text-gray-700"
+                                       for="remember">
+                                    {{ trans('auth.remember_me') }}
+                                </label>
+                            </div>
+                        </div>
+    
+                        <button
+                            class="flex items-center justify-center w-full h-8 px-3 text-sm font-semibold tracking-tight text-white transition bg-brand-600 rounded-lg shadow hover:bg-brand-500 focus:bg-brand-700 focus:outline-none focus:ring-offset-2 focus:ring-offset-brand-700 focus:ring-2 focus:ring-white focus:ring-inset"
+                            type="submit">{{ trans('auth.login') }}
+                        </button>
+                    @endif
 
                     @if($hasSsoLoginAvailable)
                         <a href="{{ route('oauth.login') }}" class="flex items-center justify-center w-full h-8 px-3 text-sm font-semibold tracking-tight text-white transition bg-brand-600 rounded-lg shadow hover:bg-brand-500 focus:bg-brand-700 focus:outline-none focus:ring-offset-2 focus:ring-offset-brand-700 focus:ring-2 focus:ring-white focus:ring-inset">
